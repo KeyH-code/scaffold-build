551af693d0c3fc1efa9da7ad88bb86f585eb4efb
keycode <3362805328@qq.com>
2026-08-13 22:01:00 +0800

    sky-common模块中的constant以及result模块构建

---
+++ sky-take-out/sky-common/src/main/java/com/sky/constant/AutoFillConstant.java
@@ -0,0 +1,14 @@
+package com.sky.constant;
+
+/*
+* 公共字段自动填充相关名称
+* */
+public class AutoFillConstant {
+    /*
+    * 实体类的方法名称
+    * */
+    public static final String SET_CREATE_TIME = "setCreateTime";
+    public static final String SET_UPDATE_TIME = "setUpdateTime";
+    public static final String SET_UPDATE_USER = "setUpdateUser";
+    public static final String SET_CREATE_USER = "setCreateUser";
+}
---
+++ sky-take-out/sky-common/src/main/java/com/sky/constant/PasswordConstant.java
@@ -0,0 +1,9 @@
+package com.sky.constant;
+
+/*
+* 默认密码常量类
+* */
+
+public class PasswordConstant {
+    public static final String DEFAULT_PASSWORD = "123456";
+}
