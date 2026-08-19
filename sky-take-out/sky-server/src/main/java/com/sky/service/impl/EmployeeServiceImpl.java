dffa39c43a5daaebbf6c90841d3c6dbb6469469c
keycode <3362805328@qq.com>
2026-08-19 18:08:27 +0800

    单元5：修复 diff 必改项（补充 DigestUtils import + 修正数据库密码配置）

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -11,6 +11,7 @@ import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
